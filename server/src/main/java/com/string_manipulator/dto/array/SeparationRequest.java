837c1a12df37e30511ba6a77d9aa4f48738b7c64
woweeclapper <joenguyener4579@gmail.com>
2026-03-07 21:35:51 -0600

    such a pepega mistake holy hell

--- server/src/main/java/com/string_manipulator/dto/array/SeparationRequest.java
+++ server/src/main/java/com/string_manipulator/dto/array/SeparationRequest.java
@@ -9,7 +9,7 @@ public record SeparationRequest(
         @NotEmpty(message = "Number list must not be empty")
         @Size(min = 1, message = "Number list must have at least one element")
         @Size(max = 1000, message = "Number list cannot exceed 1000 elements")
-        List<@NotNull(message = "Number cannot be null") Number> numberList,
+        List<@NotNull(message = "Number cannot be null") Number> numbersList,
 
         @NotBlank(message = "Separation type cannot be blank")
         @Pattern(
